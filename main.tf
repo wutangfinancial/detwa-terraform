917192037b7cfa06dd9ac33406db36ee71df9c79
Adam Salowitz <sal@detwa.com>
2018-07-07 05:38:23 +0000

    route53. provisioner running locally on tf

--- main.tf
+++ main.tf
@@ -1,16 +1,24 @@
-resource "aws_instance" "jenkins" {
+resource "aws_instance" "pipeline" {
   ami = "${var.my_ami}"
   subnet_id = "${var.my_subnet}"
   key_name = "${var.my_key}"
-  vpc_security_group_ids = ["${var.my_jenkins_secgroups}"]
+  vpc_security_group_ids = ["${var.my_pipeline_secgroups}"]
   instance_type = "t2.nano"
   disable_api_termination = true
   
   tags {
-    Name = "jenkins"
+    Name = "pipeline"
   }
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.jenkins.public_ip} > ip_address.txt"
+    command = "curl https://raw.githubusercontent.com/wutangfinancial/my_hab_bootstrap/master/bootstrap.sh | sudo bash"
   }
+}
+
+resource "aws_route53_record" "pipeline" {
+  zone_id = "${var.my_route53_zone_id}"
+  name    = "pipeline.detwah.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.pipeline.public_ip}"]
 }
